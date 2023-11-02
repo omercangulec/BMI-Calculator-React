25529258db1f17f86a1ed39bf7afc840e34eb2d9
omercangulec <omercangulec@hotmail.com>
2023-11-02 23:43:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [hegiht, setHegiht] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBMI] = useState(0);
+  const lastHeight = hegiht / 100;
+
+  function handleReset() {
+    setHegiht("");
+    setWeight("");
+    setBMI(0);
+  }
+
+  function handleCalculate(e) {
+    e.preventDefault();
+    if (!hegiht || !weight) return alert("Please enter a valid value");
+    setBMI(weight / (lastHeight * lastHeight));
+  }
+
+  const onHeight = (e) => setHegiht(+e.target.value);
+  const onWeight = (e) => setWeight(+e.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Form
+        hegiht={hegiht}
+        weight={weight}
+        onHeight={onHeight}
+        onWeight={onWeight}
+        onCalculate={handleCalculate}
+      />
+      {bmi !== 0 && <Result bmi={bmi} onReset={handleReset} />}
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <div>
+      <h1>📊 BMI Calculator</h1>
+    </div>
+  );
+}
+
+function Form({ hegiht, weight, onHeight, onWeight, onCalculate }) {
+  return (
+    <form className="form" onSubmit={onCalculate}>
+      <label>Height</label>
+      <input
+        type="number"
+        value={hegiht}
+        onChange={onHeight}
+        placeholder="Kg"
+      />
+
+      <label>Weight</label>
+      <input
+        type="number"
+        value={weight}
+        onChange={onWeight}
+        placeholder="cm"
+      />
+
+      <button>Calculate</button>
+    </form>
+  );
+}
+
+function Result({ bmi, onReset }) {
+  const textColor = `${bmi > 25 ? "red" : bmi < 23 ? "green" : "orange"}`;
+
+  return (
+    <div className="result">
+      <p>
+        Your BMI is <span style={{ color: textColor }}>{bmi.toFixed(2)}</span>
+      </p>
+      <p style={{ color: textColor }}>
+        {bmi > 25 ? "Bad" : bmi < 23 ? "Good" : "Not bad"}
+      </p>
+      <button className="btn-reset" onClick={onReset}>
+        Reset
+      </button>
+    </div>
+  );
+}
